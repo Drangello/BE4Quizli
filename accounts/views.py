09b4a87799b81311fc7b6d010744c63cb8d6a868
Drangello <drangello0505@gmail.com>
2026-06-09 14:14:41 +0200

    Implement RegisterView for user registration with validation and response handling

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(APIView):
+    """
+    Register a new user.
+    """
+
+    def post(self, request):
+        serializer = RegisterSerializer(
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                {
+                    "detail":
+                    "User created successfully!"
+                },
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
