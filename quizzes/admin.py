cef85a7353e41dbba6ce0b5b57b0bd7b8ab0ee7d
Drangello <drangello0505@gmail.com>
2026-06-10 13:09:31 +0200

    Add initial migration and admin configurations for Quiz and Question models

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Quiz
+
+
+class QuestionInline(admin.TabularInline):
+    """
+    Allows editing questions inside a quiz.
+    """
+
+    model = Question
+    extra = 0
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for quizzes.
+    """
+
+    list_display = [
+        "id",
+        "title",
+        "user",
+        "created_at",
+    ]
+    search_fields = [
+        "title",
+        "description",
+        "user__username",
+    ]
+    list_filter = [
+        "created_at",
+        "updated_at",
+    ]
+    inlines = [
+        QuestionInline,
+    ]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for questions.
+    """
+
+    list_display = [
+        "id",
+        "question_title",
+        "quiz",
+        "answer",
+    ]
+    search_fields = [
+        "question_title",
+        "answer",
+        "quiz__title",
+    ]
