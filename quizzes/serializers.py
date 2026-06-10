bf086b678aaa0ae6cc8f7c4600eaec39aaa3b7a5
Drangello <drangello0505@gmail.com>
2026-06-10 13:16:16 +0200

    Add verbose names to Quiz model and implement serializers for Quiz and Question

---
+++ quizzes/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from .models import Question, Quiz
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    """
+    Serializer for quiz questions.
+    """
+
+    class Meta:
+        model = Question
+        fields = [
+            "id",
+            "question_title",
+            "question_options",
+            "answer",
+            "created_at",
+            "updated_at",
+        ]
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    """
+    Serializer for quizzes with questions.
+    """
+
+    questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Quiz
+        fields = [
+            "id",
+            "title",
+            "description",
+            "created_at",
+            "updated_at",
+            "video_url",
+            "questions",
+        ]
