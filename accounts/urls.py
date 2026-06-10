2249b414d81e28b2d1073e6fe44f1b5639b0ad62
Drangello <drangello0505@gmail.com>
2026-06-10 12:14:13 +0200

    Add logout and token refresh URL routes in accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import LoginView, RegisterView
+from .views import LoginView, LogoutView, RegisterView
+from .views import TokenRefreshCookieView
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
     path("login/", LoginView.as_view()),
+    path("logout/", LogoutView.as_view()),
+    path("token/refresh/", TokenRefreshCookieView.as_view()),
 ]
