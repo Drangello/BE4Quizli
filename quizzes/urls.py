414bfc3ad501e65c5ba6cdcd23d24b0d4b28af67
Drangello <drangello0505@gmail.com>
2026-06-10 13:19:35 +0200

    Add QuizListCreateView and update URL routing for quizzes

---
+++ quizzes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import QuizListCreateView
+
+
+urlpatterns = [
+    path("quizzes/", QuizListCreateView.as_view()),
+]
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Quiz
+from .serializers import QuizSerializer
+
+
+class QuizListCreateView(APIView):
+    """
+    List user quizzes or create a new quiz.
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        quizzes = Quiz.objects.filter(
+            user=request.user
+        ).order_by("-created_at")
+        serializer = QuizSerializer(quizzes, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
